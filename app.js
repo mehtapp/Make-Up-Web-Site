5d93eb90b196b8576260b8a243f16ac64286bdac
mehtapp <41346509+mehtapp@users.noreply.github.com>
2022-03-27 13:31:24 +0300

    initialize

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const Port=process.env.PORT||3000; //for heroku
 const userRoute=require("./Routes/user");
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(8000);
+app.listen(Port);
 app.use(userRoute);
 
